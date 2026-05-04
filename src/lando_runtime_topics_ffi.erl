7dc50badc6ebbc8c2566562e022c2faa3f68667c
Dave Rapin <dave@rapin.com>
2026-05-03 23:35:53 -0400

    Add pg-based topic infrastructure for broadcast
    OTP pg process groups for topic pub/sub: join, leave, members,
    broadcast (excludes sender), receive_frame. Erlang FFI + Gleam
    wrapper + tests. Added gleam_erlang as direct dependency.

---
+++ src/lando_runtime_topics_ffi.erl
@@ -0,0 +1,37 @@
+-module(lando_runtime_topics_ffi).
+-export([start/0, join/1, leave/1, members/1, broadcast/2, receive_frame/1]).
+
+start() ->
+    case pg:start_link(lando_topics) of
+        {ok, _Pid} -> nil;
+        {error, {already_started, _Pid}} -> nil
+    end.
+
+join(Topic) ->
+    pg:join(lando_topics, Topic, self()),
+    nil.
+
+leave(Topic) ->
+    pg:leave(lando_topics, Topic, self()),
+    nil.
+
+members(Topic) ->
+    pg:get_members(lando_topics, Topic).
+
+broadcast(Topic, Frame) ->
+    Members = pg:get_members(lando_topics, Topic),
+    Self = self(),
+    lists:foreach(fun(Pid) ->
+        case Pid of
+            Self -> ok;
+            _ -> Pid ! {lando_push, Frame}
+        end
+    end, Members),
+    nil.
+
+receive_frame(TimeoutMs) ->
+    receive
+        {lando_push, Frame} -> {ok, Frame}
+    after TimeoutMs ->
+        {error, timeout}
+    end.
